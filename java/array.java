368e2eaf4d9b6716fc27572ba79af03ddbc022b7
abinaya-udayabanu <34646478+abinaya-udayabanu@users.noreply.github.com>
2018-03-10 20:28:49 +0530

    Create array.java

---
+++ java/array.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int k=in.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=in.nextInt();
+		}
+		int s=0;
+		for(int i=0;i<k;i++)
+		{
+			s=s+a[i];
+		}
+	            
+
+		System.out.print(s);
+	}
+}
